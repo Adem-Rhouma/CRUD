0d99ef283820c7be910db02bebd48ed83ca1fdc4
Adem-Rhouma <ademrhouma077@gmail.com>
2021-10-03 17:53:28 +0100

    OOP Crud

--- Crud_test.py
+++ Crud_test.py
@@ -1,8 +1,8 @@
 import random
-personalisation = []
+
 option=input('would u like to create or read or update or delete: ')
 i=1
-
+personalisation = []
 class person():
 
     def __init__(self):
@@ -13,9 +13,22 @@ class person():
         self.name = name
         self.last_name = last_name
         self.ID = ID
+        while name[0].upper() != name[0] or last_name[0].upper() != last_name[0]:
+            print("Invalid name: first letter needs to be an upper letter")
+            a.create(input('Name: '), input("last name: "), 321)
+        print(ID)
+        print(a.create)
+        personalisation.append(a.create)
+        print(personalisation[0].name)
+        
+        
 
     def read(self, ID_read):
         self.ID_read=ID_read
+        print(personalisation[0].ID)
+        b=personalisation.index(ID_read)
+        print('test')
+        print(b)
 
 
 
@@ -31,21 +44,17 @@ class person():
 
 
 
-
+a=person()
+xlist=[]
 x=random.randint(10000000, 99999999)
+while x in xlist:
+    x=random.randint(10000000, 99999999)
+xlist.append(x)
 if option == 'create':
-    a=person()
-    a.create(input('Name: '), input('Last name: '), 321)
-    while a.name[0].upper() != a.name[0] or a.last_name[0].upper() != a.last_name[0]:
-        print("Invalid name: first letter needs to be an upper letter")
-        a=person(input('Name: '), input("last name: "), 321)
-    personalisation.append(a)
-    print(personalisation[0].ID)
+    a.create(input('name: '), input('last name: '), 321)
     option=input('would u like to create or read or update or delete: ')
-
 
 if option == 'read':
     a.read(input('ID: '))
-    print(personalisation.index('Adem'))
         
     
